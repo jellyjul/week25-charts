8818999cf095d44aaa203ddbe788e4e5d21cbe28
jellyjul <100496@mail.ru>
2023-03-23 14:34:39 +0500

    second

--- app_01/src/App.js
+++ app_01/src/App.js
@@ -1,35 +1,30 @@
 import React from "react";
 import { Chart } from "react-google-charts";
-import "./App.css";
 
-const data = [
-  ["Year", "Sales", "Expenses", "Profit"],
-  ["2016", 1000, 400, 200],
-  ["2017", 1100, 600, 250],
-  ["2018", 900, 300, 300],
-  ["2019", 1200, 1000, 200],
-  ["2020", 800, 100, 400],
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Work", 11],
+  ["Eat", 2],
+  ["Commute", 2],
+  ["Watch TV", 2],
+  ["Sleep", 7],
 ];
 
-const options = {
-  chart: {
-    title: "Company Performance",
-    subtitle: "Sales, Expenses, and Profit: 2016-2020",
-  },
+export const options = {
+  title: "My Daily Activities",
 };
 
-const App = () => {
+export function App() {
   return (
-    <div>
-      <Chart
-        chartType="Bar"
-        data={data}
-        options={options}
-        width="80%"
-        height="500px"
-      />
-    </div>
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
   );
-};
+}
+
 
 export default App;
